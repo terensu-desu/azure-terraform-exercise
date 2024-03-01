62ac87b8e926c9b067bdcac9193725fe84911bfc
Terence 'TMan' Mangram <tmangram@gmail.com>
2024-03-01 13:37:20 -0600

    feat: add resource group and application name values

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,14 +1,14 @@
 variable "resource_group" {
   description = "The resource group"
-  default = "CHANGE_ME_RESOURCE_GROUP"
+  default = "azz-terraform-apps"
 }
 
 variable "application_name" {
   description = "The Spring Boot application name"
-  default     = "CHANGE_ME_APP_NAME"
+  default     = "azure-teraform-exercise"
 }
 
 variable "location" {
   description = "The Azure location where all resources in this example should be created"
-  default     = "westeurope"
+  default     = "eastus"
 }
